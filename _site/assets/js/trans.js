a20f5e7a5904d63f92988ee31101cafdd1d0728d
Andrew Downs <druzydowns@gmail.com>
2021-01-06 00:29:16 -0500

    change mobile breakpoint of index.html to use lightbox library, refactor trans.js

--- _site/assets/js/trans.js
+++ _site/assets/js/trans.js
@@ -1,80 +1,83 @@
-const options = {
-  containers: ["#left", "#right", "#full"]
+const swupOptions = {
+  containers: ["#left", "#right", "#full"],
 };
-const swup = new Swup(options);
+const swup = new Swup(swupOptions);
 
 function imgHover() {
-    if (window.location.pathname == '/') {
-    var img = document.querySelector("img.frame-img");
-    var imgMobileContainer = document.querySelector("div.frame-img-mobile-container");
-    var imgMobile = document.querySelector("img.frame-img-mobile");
-    var posterContainer = document.querySelector("div.poster-container");
-    var hover = document.querySelectorAll("a.hover-img");
+  var breakpoint;
+  var getBreakpoint = function () {
+    return window
+      .getComputedStyle(document.body, ":before")
+      .content.replace(/\"/g, "");
+  };
+
+  breakpoint = getBreakpoint();
+
+  window.addEventListener(
+    "resize",
+    function () {
+      breakpoint = getBreakpoint();
+    },
+    false
+  );
+
+  if (window.location.pathname === "/") {
     var workNumber = document.querySelectorAll("span.work-number");
+    for (let ii = 0; ii < workNumber.length; ii++) {
+      workNumber[ii].innerHTML = ii;
+    }
+    // Breakpoint Environments
+    if (breakpoint != "small") {
+      var imgList = document.querySelector("div.poster-container");
+      var hover = document.querySelectorAll("a.hover-img");
+      var img = document.querySelector("img.frame-img");
 
-    for (let i = 0; i < hover.length; i++) {
+      for (let i = 0; i < hover.length; i++) {
         hover[i].addEventListener("mouseover", function () {
-            img.src = `/assets/img/portfolio/posters/${hover[i].dataset.img}`;
-            imgMobile.src = `/assets/img/portfolio/posters/${hover[i].dataset.img}`;
-            imgMobileContainer.classList.toggle('closed');
-        });
-        
-        hover[i].addEventListener("mouseleave", function () {
-            img.src = `/assets/img/po.png`;
-            imgMobile.src = `/`;
-            imgMobile.classList.toggle('closed');
+          img.src = `/assets/img/portfolio/posters/${hover[i].dataset.img}`;
         });
 
-
-        hover[i].addEventListener("touch", function () {
-            imgMobile.src = ``;
-            imgMobile.src = `/assets/img/portfolio/${hover[i].dataset.img}`;
-            imgMobileContainer.classList.toggle('closed');
-            posterContainer.classList.toggle('pointer-none');
+        imgList.addEventListener("mouseleave", function () {
+          img.src = `/assets/img/po.png`;
         });
-        
-        imgMobile.addEventListener("mouseleave", function () {
-            posterContainer.classList.remove('pointer-none');
-            imgMobileContainer.classList.toggle('closed');
-        });
-
+      }
+    } else {
+      const lightbox = GLightbox({
+        touchNavigation: true,
+        loop: true,
+      });
     }
-    for (let j = 0; j < workNumber.length; j++) {
-        workNumber[j].innerHTML = j;
-    }
+  }
 }
-};
 
 function navHighlight() {
-    const posters = document.getElementById('posters');
-    const uf = document.getElementById('uf');
-    const cade = document.getElementById('cade');
-    switch (window.location.pathname) {
-        case "/":
-            posters.classList.toggle('active');
-            uf.classList.toggle('inactive');
-            cade.classList.toggle('inactive');
-            break;
-        case "/uf":
-            posters.classList.toggle('inactive');
-            uf.classList.toggle('active');
-            cade.classList.toggle('inactive');
-            break;
-        case "/cade":
-            posters.classList.toggle('inactive');
-            uf.classList.toggle('inactive');
-            cade.classList.toggle('active');
-            break;
-        default:
-            break;
-    }
-};
-
-
+  const posters = document.getElementById("posters");
+  const uf = document.getElementById("uf");
+  const cade = document.getElementById("cade");
+  switch (window.location.pathname) {
+    case "/":
+      posters.classList.toggle("active");
+      uf.classList.toggle("inactive");
+      cade.classList.toggle("inactive");
+      break;
+    case "/uf":
+      posters.classList.toggle("inactive");
+      uf.classList.toggle("active");
+      cade.classList.toggle("inactive");
+      break;
+    case "/cade":
+      posters.classList.toggle("inactive");
+      uf.classList.toggle("inactive");
+      cade.classList.toggle("active");
+      break;
+    default:
+      break;
+  }
+}
 
 imgHover();
 navHighlight();
 
 // this event runs for every page view after initial load
-swup.on('contentReplaced', imgHover);
-swup.on('contentReplaced', navHighlight);
+swup.on("contentReplaced", imgHover);
+swup.on("contentReplaced", navHighlight);
